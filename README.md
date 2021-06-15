ecaf19d3e4a54b3c9826b3f272ec26f78c58eaef
garylyp <garylyp@hotmail.com>
2021-06-16 01:22:37 +0800

    Fix tests

--- README.md
+++ README.md
@@ -11,6 +11,10 @@
 2. In the root directory of the clone, run the command `install` on a shell.
 3. Once the installation is completed, run the command `run` and go to `http://127.0.0.1:8050`.
 
+## Instructions to Test
+
+1. In the root directory, run the command `test`.
+
 ## Developer
 
 Please help this poor man develop. Tech Stack used: Bootstrap, Dash, Plotly.
--- test/services/test_data_service.py
+++ test/services/test_data_service.py
@@ -2,17 +2,25 @@ import unittest
 import pandas as pd
 import pandas.testing as pt
 from src.services import data_service
+from src.assets import constants
 
 class TestDataService(unittest.TestCase):
     test_bundle_data = ''
 
     def setUp(self):
         self.test_bundle_data = pd.read_csv('.\\test\\data\\test_bundle_1.csv')
+        # By right this should be done in parser
+        columns_to_rename = {
+            'Tags':'Tag', 
+            'Duration (decimal)':'Time (decimal)', 
+            'Duration (h)':'Time (h)'
+        }
+        self.test_bundle_data.rename(columns=columns_to_rename, inplace=True)
 
     def test_sum_time_by_tag(self):
-        sum_time_by_tag = data_service.sum_time_by_tag(self.test_bundle_data)
+        sum_time_by_tag = data_service.sum_time_by_criteria(self.test_bundle_data, ["Tag"])
         expected_df = pd.DataFrame({
-            'Tags': [
+            'Tag': [
                 'Downtime (Watching Shows, Gaming, Resting, Taking a Break)',
                 'Exercising (Run, Swim, Workout)',
                 'Hobbies (Music, Cooking)',
@@ -23,7 +31,7 @@ class TestDataService(unittest.TestCase):
                 'Socialising (Chatting, Hanging Out)',
                 'Surviving (Groceries, Eating, Hygiene)'
             ],
-            'Duration (decimal)': [
+            'Time (decimal)': [
                 7.63, 1.51, 3.42, 1.20, 13.46, 4.93, 23.88, 1.20, 7.66
             ]
         })
