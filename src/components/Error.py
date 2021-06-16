286b7f157720da80564072524992f1e147a2f3ab
t-cheepeng <tcp97@hotmail.com>
2021-06-16 16:04:35 +0800

    Correct typo in message

--- src/components/Error.py
+++ src/components/Error.py
@@ -1,3 +1,3 @@
 import dash_html_components as html
 
-layout = html.H4(['There was error processing the file uploaded or the file was empty.'])
+layout = html.H4(['There was an error processing the file uploaded or the file was empty.'])
