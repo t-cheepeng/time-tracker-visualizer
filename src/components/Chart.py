3c00ddeddfc8588d2aaacf7e5eeeb134fe63f3b8
t-cheepeng <tcp97@hotmail.com>
2021-07-05 18:01:59 +0800

    Shift groupby and add workweek functionality

--- src/components/Chart.py
+++ src/components/Chart.py
@@ -2,53 +2,70 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 import plotly.express as px
 
 from app import app
-from services import data_service, time_service
+from components import GroupBy
+from services import data_service, parser_service, time_service
 import constants as const
 
-def serve_layout(parsed_data, criteria):
-    criteria_sums = data_service.sum_time_by_criteria(parsed_data, criteria)
-    total_time = data_service.sum_time(criteria_sums)
-    
-    fig = px.pie(criteria_sums, values=const.time_in_decmial,
-                names=criteria[0], title='An Averaged 24hr')
-    fig.update_traces(text=[time_service.convert_to_hrs_for_display(
-        criteria_sums.iloc(0)[i][1], total_time) for i in range(0, len(criteria_sums))])
+layout = html.Div([
+    GroupBy.layout,
+    dbc.Row([
+        dbc.Col([
+            dcc.Dropdown(
+                id='dropdown-selector',
+                options=[
+                    {'label': 'Averaged', 'value': 'Averaged'},
+                    {'label': 'Weekday', 'value': 'Weekday'},
+                    {'label': 'Weekend', 'value': 'Weekend'}
+                ],
+                value='Averaged',
+                multi=False,
+                searchable=False,
+                placeholder='Select Chart Type...'
+            ),
+        ], width=4, className='p-3'),
+    ]),
+    dbc.Row([
+        dbc.Col(id='chart-container')
+            ])
+])
 
-    return html.Div([
-        dbc.Row([
-            dbc.Col([
-                dcc.Dropdown(
-                    id='dropdown-selector',
-                    options=[
-                        {'label': 'Averaged', 'value': 'Averaged'},
-                        {'label': 'Weekday', 'value': 'Weekday'},
-                        {'label': 'Weekend', 'value': 'Weekend'}
-                    ],
-                    value='Averaged',
-                    multi=False,
-                    searchable=False,
-                    placeholder='Select Chart Type...'
-                ),
-            ], width=4, className='p-3'),
-        ]),
-        dbc.Row([
-            dbc.Col(id='chart-container', children=[dcc.Graph(figure=fig)])
-        ])
-    ])
 
 @app.callback(Output('chart-container', 'children'),
-              Input('dropdown-selector', 'value'))
-def update_chart(selected):
-    if selected == 'Averaged':
-        # TODO: Return back correct charts
-        return []
-    elif selected == 'Weekday':
-        return []
-    elif selected == 'Weekend':
-        return []
-    else:
-        return []
+              Input('dropdown-selector', 'value'),
+              Input('memory-store', 'data'),
+              Input('group-by-criteria', 'value'))
+def update_chart(selected, data, criteria):
+    if data is None:
+        # No valid data in memory store
+        raise PreventUpdate
+    
+    df = parser_service.parse_json_to_df(data)
+    fig = None
+    group_sum = None
 
+    if selected == 'Averaged':
+        group_sum = data_service.sum_time_by_criteria(
+            df, criteria)
+        fig = px.pie(group_sum, values=const.time_in_decmial,
+                 names=criteria[0], title='An Averaged 24hr')
+    elif selected == 'Weekday':
+        group_sum = data_service.sum_time_by_workweek(df, True)
+        fig = px.pie(group_sum, values=const.time_in_decmial,
+                     names=criteria[0], title='An Averaged Weekday')
+    elif selected == 'Weekend':
+        group_sum = data_service.sum_time_by_workweek(df, False)
+        fig = px.pie(group_sum, values=const.time_in_decmial,
+                     names=criteria[0], title='An Averaged Weekend')
+    else:
+        return None
+
+    total_time = data_service.sum_time(group_sum)
+    # Upate labels from decimal to hours
+    fig.update_traces(text=[time_service.convert_to_hrs_for_display(
+        group_sum.iloc(0)[i][1], total_time) for i in range(0, len(group_sum))])
+
+    return dcc.Graph(figure=fig)
--- src/components/Upload.py
+++ src/components/Upload.py
@@ -27,20 +27,18 @@ layout = dcc.Upload(
 
 @app.callback(Output('output-data-upload', 'children'),
               Output('upload-data', 'style'),
+              Output('memory-store', 'data'),
               Input('upload-data', 'contents'),
-              Input('group-by-criteria', 'value'),
               Input('upload-data', 'style'),
               State('upload-data', 'filename'))
-def update_visualizer(content, criteria, upload_box, filename):
+def update_visualizer(content, upload_box, filename):
     if content is not None:
         parsed = parser_service.parse_data(content, filename)
-        # TODO: check if df here has been renamed in columns
-        # TODO: store data in dcc.Store
         if parsed.empty:
-            return Error.layout, upload_box
+            return Error.layout, upload_box, None
         else:
             upload_box_update = upload_box.copy()
             upload_box_update['background'] = '#b4ffb4' # faded green
-            return Chart.serve_layout(parsed, criteria), upload_box_update
+            return Chart.layout, upload_box_update, parser_service.parse_df_to_json(parsed)
 
-    return None, upload_box
+    return None, upload_box, None
